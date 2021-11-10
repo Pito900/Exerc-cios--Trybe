02455e7cec275e9206380b458fafaa5269b47db6
Francisco Monteiro <franciscorubenilto@gmail.com>
2021-11-10 15:11:07 -0300

    Tirei alguns comentários

--- ExBloco4/ExLP.js
+++ ExBloco4/ExLP.js
@@ -1,49 +1,49 @@
 //1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer
 //, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
-// let ast = '*';
-// let n = 5;
-// let linha = ast.repeat(n)
-// for (i = 1; i<= n; i++) {
-//     console.log(linha);
-// }
+let ast = '*';
+let n = 5;
+let linha = ast.repeat(n)
+for (i = 1; i<= n; i++) {
+    console.log(linha);
+}
 
 ////////////////////////////////////////////////////////////////////////////////////
 //2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
 
-// let ast = '*';
-// let base = 5;
+let ast = '*';
+let base = 5;
 
-// for (i = 1; i<= base; i++) {
-//     console.log(ast.repeat(i))
-// }
+for (i = 1; i<= base; i++) {
+    console.log(ast.repeat(i))
+}
 
 /////////////////////////////////////////////////////////////////////////////////////
 //3- Agora inverta o lado do triângulo.
 
-// let ast = '*';
-// let vazio = ' ';
-// let base = 5;
+let ast = '*';
+let vazio = ' ';
+let base = 5;
 
-// for (i = 1; i <= base; i++) {
-//     console.log(vazio.repeat(base-i) + ast.repeat(i))
-// }
+for (i = 1; i <= base; i++) {
+    console.log(vazio.repeat(base-i) + ast.repeat(i))
+}
 // aqui vai uma segunda forma de fazer (do solution)
-// let n = 5;
-// let symbol = '*';
-// let inputLine = '';
-// let inputPosition = n;
-// //o primeiro for corresponde as linhas e o segundo as colunas (cada item da linhas)
-// for (let lineIndex = 0; lineIndex < n; lineIndex += 1) { 
-//   for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
-//     if (columnIndex < inputPosition) {
-//       inputLine = inputLine + ' ';
-//     } else {
-//       inputLine = inputLine + symbol;
-//     }
-//   }
-//   console.log(inputLine);
-//   inputLine = ''; // aqui resetou-se o imputline
-//   inputPosition -= 1; // aqui retiramos um do inputposition para que o triangulo cresça inversamente..caso contrário apareceria só a ultima coluna.
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+let inputPosition = n;
+//o primeiro for corresponde as linhas e o segundo as colunas (cada item da linhas)
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) { 
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      inputLine = inputLine + ' ';
+    } else {
+      inputLine = inputLine + symbol;
+    }
+  }
+  console.log(inputLine);
+  inputLine = ''; // aqui resetou-se o imputline
+  inputPosition -= 1; // aqui retiramos um do inputposition para que o triangulo cresça inversamente..caso contrário apareceria só a ultima coluna.
 
 
 //////////////////////////////////////////////////////////////////////////////////////
@@ -52,27 +52,27 @@
 
 //OBS: vamos fazer do mesmo modo que no solution.
 
-// let n = 5;
-// let symbol = '*';
-// let inputLine = '';
+let n = 5;
+let symbol = '*';
+let inputLine = '';
 
-// let midOfMatrix = (n + 1) / 2;
-// let controlLeft = midOfMatrix;
-// let controlRight = midOfMatrix;
+let midOfMatrix = (n + 1) / 2;
+let controlLeft = midOfMatrix;
+let controlRight = midOfMatrix;
 
-// for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
-//   for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
-//     if (columnIndex > controlLeft && columnIndex < controlRight) {
-//       inputLine = inputLine + symbol;
-//     } else {
-//       inputLine = inputLine + ' ';
-//     }
-//   }
-//   console.log(inputLine);
-//   inputLine = '';
-//   controlRight += 1;
-//   controlLeft -= 1
-// };
+for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex > controlLeft && columnIndex < controlRight) {
+      inputLine = inputLine + symbol;
+    } else {
+      inputLine = inputLine + ' ';
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  controlRight += 1;
+  controlLeft -= 1
+};
 
 // Com essa resposta a primeira coluna será composta só de inputline
 //Assim controlR vai para 4 e a controlL cai para 2...dai vai por o primeiro asterisco no meio
--- ExBloco4/ExPP.js
+++ ExBloco4/ExPP.js
@@ -1,31 +1,31 @@
 //Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
 
-// const valorA = 1;
-// const valorB = 2;
-// const valorC = 3;
+const valorA = 1;
+const valorB = 2;
+const valorC = 3;
 
-// if (valorA > valorB && valorA > valorC) {
-//     console.log(valorA)
-// } else if (valorB > valorA && valorB > valorC) {
-//     console.log(valorB)
-// } else if (valorC > valorA && valorC > valorB) {
-//     console.log(valorC)
-// } else {
-//     console.log("Nda a fazer.")
-// }
+if (valorA > valorB && valorA > valorC) {
+    console.log(valorA)
+} else if (valorB > valorA && valorB > valorC) {
+    console.log(valorB)
+} else if (valorC > valorA && valorC > valorB) {
+    console.log(valorC)
+} else {
+    console.log("Nda a fazer.")
+}
 
 //////////////////////////////////////////////////////////////////////////
 //Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
 
-// const valorA = 6;
-// const valorB = 2;
-// const valorC = 4;
+const valorA = 6;
+const valorB = 2;
+const valorC = 4;
 
-// let numero = false;
-// if  (valorA%2 ===0 || valorB%2 ===0 || valorA%2 ===0 ) {
-//     numero = true
-// }
+let numero = false;
+if  (valorA%2 ===0 || valorB%2 ===0 || valorA%2 ===0 ) {
+    numero = true
+}
 
-// console.log(numero)
+console.log(numero)
 
 /////////////////////////////////////////////////////////
