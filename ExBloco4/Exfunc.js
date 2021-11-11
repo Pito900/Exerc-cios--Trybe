18413eed7ac53b56c942fb09978b53f9aa04bfad
Francisco Monteiro <franciscorubenilto@gmail.com>
2021-11-11 10:35:56 -0300

    Resolvi os exercícios bonus

--- ExBloco4/Exfunc.js
+++ ExBloco4/Exfunc.js
@@ -121,3 +121,125 @@ function maisRepetido(numeros) {
     return controle;
   } //no exemplo da trybe no primeiro for ele verifica se o elemento 3 (da palavra) é igual ao indice 0 do fim da palavra. 
   // e assim or diante...Se aumento um indice for diferente a função já retorna false
+
+/////////////////////////////////////////////////////////////////////////////////
+//////////////////////BONUS//////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+//2 - Crie uma função chamada arrayOfNumbers que receberá a variável vector como parâmetro. Através de um loop for , percorra essa variável, 
+//busque os números pares e os adicione a um novo array que deverá ser retornado ao final pela pela função.
+
+function arrayOFNumbers(vector) {
+  let nPares = [];
+  for (let i = 0; i < vector.length; i++) {
+    for (let j of vector[i]) {
+      if (j % 2 === 0) {
+        nPares.push(j);
+      };
+    };
+  };
+  return (console.log(nPares));
+};
+arrayOFNumbers([[1, 2], [3,4,5,6], [7,8,9,10]]);
+
+//3 - A partir do array de frutas basket , retorne um objeto que contenha o nome da fruta como chave e a quantidade de vezes que ela aparece
+//no array como valor. Por exemplo, o array ['Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva'], deverá retornar { Melancia: 3, Abacate: 
+//1, Uva: 1 } quando passado como argumento para a função. Em seguida, imprima esse resultado na tela com uma mensagem no seguinte formato: 
+//Sua cesta possui: x Melancias, x Abacates...
+let cesta = {}
+function func(basket) {
+  for (let i of basket) {
+    let cont = 0;
+    for (let j of basket){
+      if (j === i) {
+        cont++;
+      };
+    };
+    cesta[i] = cont;
+  };
+  return (cesta)
+};
+
+
+
+func([
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+])
+
+let showingCesta = [];
+for (let key in cesta){
+  showingCesta.push(cesta[key] + ' ' + key);
+}
+console.log("Sua cesta possui: " + showingCesta.join(", "));
+
+//Usando o objeto abaixo, faça os exercícios a seguir:
+
+ let moradores = {
+  blocoUm: [
+    {
+      nome: 'Luiza',
+      sobrenome: 'Guimarães',
+      andar: 10,
+      apartamento: 1005,
+    },
+    {
+      nome: 'William',
+      sobrenome: 'Albuquerque',
+      andar: 5,
+      apartamento: 502,
+    },
+  ],
+  blocoDois: [
+    {
+      nome: 'Murilo',
+      sobrenome: 'Ferraz',
+      andar: 8,
+      apartamento: 804,
+    },
+    {
+      nome: 'Zoey',
+      sobrenome: 'Brooks',
+      andar: 1,
+      apartamento: 101,
+    },
+  ],
+};
+
+
+//4 - Acesse as chaves nome , sobrenome , andar e apartamento do último morador do blocoDois e faça um console.
+//log no seguinte formato: "O morador do bloco 2 de nome Zoey Brooks mora no 1° andar, apartamento 101".
+
+
+// console.log(" O morador do bloco 2 de nome " + moradores.blocoDois[1].nome + "  " + moradores.blocoDois[1].sobrenome
+// + " mora no andar " + moradores.blocoDois[1].andar + " apartamento " + moradores.blocoDois[1].apartamento);
+
+//5 - Utilize o for para imprimir o nome completo de todos os moradores do bloco 1, acessando suas chaves nome e sobrenome , 
+//depois faça o mesmo para os moradores do bloco 2.
+
+
+//Para o bloco 1
+for(let i = 0; i < moradores.blocoUm.length; i++){
+      console.log(moradores.blocoUm[i]["nome"] + ' ' + moradores.blocoUm[i]["sobrenome"])
+}
+
+//Para o bloco 2
+
+for(let i = 0; i < moradores.blocoDois.length; i++){
+  console.log(moradores.blocoDois[i]["nome"] + ' ' + moradores.blocoDois[i]["sobrenome"])
+}
--- ExBloco4/ObjForIn.js
+++ ExBloco4/ObjForIn.js
@@ -82,4 +82,3 @@ console.log(leitor)
 
 //8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
 console.log(leitor.nome + " tem 2 livros favoritos")
-
